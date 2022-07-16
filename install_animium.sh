e47e503ef29ffc6c954371b833669359c78baab6
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-16 13:19:24 -0400

    Update install_animium.sh

--- install_animium.sh
+++ install_animium.sh
@@ -1,6 +1,15 @@
 echo Installing Animium...
 git clone --single-branch --branch chromeos https://github.com/KiaWeb/Animium.git
-echo Installing some npm packages...
+echo Installing some packages...
+curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+echo Using Node.js
+node -v
+echo Using npm version
+npm -v
+echo Peforming an npm upgrade...
+npm install -g npm@8.14.0
+echo now back to installing packages...
 cd Animium
 npm install
 npm install kyriosli/node-zip
