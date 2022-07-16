f30732dc2b7930cdade7fc938049d4331c4c4470
josephanimate2021 <100003036+josephanimate2021@users.noreply.github.com>
2022-07-16 12:35:58 -0400

    Update install_animium.sh

--- install_animium.sh
+++ install_animium.sh
@@ -2,5 +2,6 @@ echo Installing Animium...
 git clone --single-branch --branch chromeos https://github.com/KiaWeb/Animium.git
 echo Installing some npm packages...
 cd Animium
-sudo bash ./installDepends.sh
+npm install
+npm install kyriosli/node-zip
 echo "Animium is sucessfully installed!"
